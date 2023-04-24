e8edcd54ddf455f38f952ff57ee02ac7ccb08386
spiritual <venommortalx@gmail.com>
2023-04-24 13:54:30 -0400

    learn how functions work

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import requests
-import os.path
+import os
+import sys
 import json
 import cloudscraper
 
-config = {
-    "cookie": "",
-    "delay": 0
-}
-config_json = "config.json"
-daily_case = "https://csgocases.com/case/daily-free"
-request_module = requests
+global config 
+##global daily_case = "https://csgocases.com/case/daily-free"
+##global request_module = requests
 
-if os.path.isfile(config_json) != True:
-    ws = open(config_json, "w")
-    config["cookie"] = input("Paste in the login cookie")
-    ws.write(json.dumps(config))
-    ws.close()
+def create_config():
+    config = {
+        "cookie": "",
+        "delay": 0
+    }
+        
+    if os.path.isfile("config.json") != True:
+        config["cookie"] = input("Please paste in the login cookie: ")
+        if config["cookie"] != "":
+            ws = open("config.json", "w")
+            ws.write(json.dumps(config))
+            ws.close()
+        else:
+            os.execl(sys.executable, sys.executable, *sys.argv)
+    else: 
+        rs = open("config.json", "r")
+        config = json.loads(rs.read())
+        print(config)
 
-    rs = open(config_json, "r")
-    config = json.loads(rs.read())
-else: 
-    config = json.loads(open("config.json", "r").read())
-    print(config)
+create_config()
