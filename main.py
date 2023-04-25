51f3a98831b3f424004e829e6db5e2eb183c53ae
Sekihotai <38195453+Sekihotai@users.noreply.github.com>
2023-04-25 14:06:08 -0400

    add webhook function and begin prettifying main process function

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import time
+import requests
 import undetected_chromedriver as uc
 from selenium.webdriver.remote.webdriver import By
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-def open_cases():
+def main_process():
     # name_case = "https://csgocases.com/case/daily-free"
     # avatar_case = "https://csgocases.com/case/daily-free-2"
-    options = uc.ChromeOptions()
-    options.user_data_dir = "C:/Users/Andrew/AppData/Local/Google/Chrome/User Data"
-    options.add_argument("profile-directory=Profile 1")
-    driver = uc.Chrome(options=options)
+    def webdriver_setup():
+        options = uc.ChromeOptions()
+        options.user_data_dir = "C:/Users/Andrew/AppData/Local/Google/Chrome/User Data"
+        options.add_argument("profile-directory=Profile 1")
+        driver = uc.Chrome(options=options)
+
+    def open_cases():
+        
+
+    
     driver.get("https://csgocases.com/case/ez-1-100-profit")
     WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.ID, "lotteryStart"))
@@ -22,13 +29,47 @@ def open_cases():
             (By.XPATH, "//h1[@class='ng-scope']//span[1]"))
     )
     reward = driver.find_element(By.XPATH, "//h1[@class='ng-scope']//span[1]")
-    print(reward.text)
+    send_webhook("", reward, "", "")
     time.sleep(120)
 
-def send_webhook():
+def send_webhook(case, reward, status, thumbnail):
+    webhook_url = "https://discord.com/api/webhooks/1004215996908843060/hAbD6iuAl1_qHUkjHJxAWyhkulX5AYrwS3abvSXSPCsW7pivUeKYFB6GkFNtrigwswKv"
+
+    data = {
+        "content": null,
+        "embeds": [
+        {
+            "title": case,
+            "url": "https://csgocases.com/r/u70633zei",
+            "color": 16774400,
+            "fields": [
+                        {
+                            "name": "Status",
+                            "value": status
+                        },
+                        {
+                            "name": "Reward",
+                            "value": reward
+                        }
+                    ],
+            "footer": {
+                "text": "spiritual is stupid"
+            },
+      "timestamp": "2023-04-11T19:23:00.000Z",
+      "thumbnail": {
+        "url": thumbnail
+        }
+        }
+        ],
+        "attachments": []
+    }
+
+
+
+
     
 
 
 
 
-open_cases()
+main_process()
