199f0f48e75f4fb1c183618a5b719539a63eca46
Sekihotai <venommortalx@gmail.com>
2023-04-28 17:01:35 -0400

    close to final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 import undetected_chromedriver as uc
 from selenium.webdriver.remote.webdriver import By
 import selenium.webdriver.support.expected_conditions as EC
@@ -8,6 +9,7 @@ def main_process():
     def open_cases(url): 
         options = uc.ChromeOptions()
         options.user_data_dir = "C:/Users/Andrew/AppData/Local/Google/Chrome/User Data"
+        options.add_argument("--headless")
         options.add_argument("profile-directory=Profile 1")
         driver = uc.Chrome(options=options)
 
@@ -19,8 +21,7 @@ def main_process():
 
         case = driver.find_element(By.TAG_NAME, 'h1').text
         thumbnail = driver.find_element(By.CLASS_NAME, 'case-img').get_attribute("src")
-        
-        driver.find_element((By.ID, "lotteryStart")).click()
+        driver.find_element(By.ID, "lotteryStart").click()
         WebDriverWait(driver, 20).until(
             EC.visibility_of_element_located(
                 (By.XPATH, "//h1[@class='ng-scope']//span[1]"))
@@ -28,15 +29,18 @@ def main_process():
 
         reward = driver.find_element(By.XPATH, "//h1[@class='ng-scope']//span[1]").text
 
+        driver.find_element(By.XPATH, "//div[@id='main-wrapper']/section[1]/div[1]/div[3]/div[2]/div[1]").click()
+        balance = driver.find_element(By.ID, "walletFunds").text
+
         driver.quit()
-        send_webhook(case, reward, thumbnail)
+        send_webhook(case, reward, thumbnail, balance)
 
     open_cases("https://csgocases.com/case/daily-free")
     open_cases("https://csgocases.com/case/daily-free-2")
+    
 
-def send_webhook(case, reward, thumbnail):
+def send_webhook(case, reward, thumbnail, balance):
     webhook_url = "https://canary.discord.com/api/webhooks/1100522199607164938/6sVgPu3_O8rKNCFy5KZcvAeWRVJnvDJ2xvKrbYZ6G99PQENOHhqGjQZ3qqS3gBLLjerw"
-
     data = {
         "embeds": [
             {
@@ -57,6 +61,14 @@ def send_webhook(case, reward, thumbnail):
         "attachments": []
     }
 
+    if "Free $" in reward:
+        balance_data = {
+            "name": "New Balance",
+            "value": balance
+        }
+        data["embeds"][0]["fields"].append(balance_data)
+
+    
     requests.post(webhook_url, json=data)
 
 main_process()
